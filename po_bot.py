660766d621cf83474af75527ec111435fcb68293
Adrian Kim <adriankim0722@gmail.com>
2022-05-17 15:54:26 +0900

    added dog for flavor

--- po_bot.py
+++ po_bot.py
@@ -1,15 +1,19 @@
 from po_crawl import auto_po
 from config import yt_id, yt_pw, vh_id, vh_pw
+from retriever import retriever_art
 
-overseas_client_dict = {
-    "lgd_yeontai": {"id": yt_id, "pw": yt_pw, "identity": "yeontai"}, 
-    "lgd_vh": {"id": vh_id, "pw": vh_pw, "identity": "vaihong"}
-}
+overseas_client_list = [
+    {"id": yt_id, "pw": yt_pw, "identity": "yeontai"}, 
+    {"id": vh_id, "pw": vh_pw, "identity": "vaihong"}
+]
 
 
 def michael_schumacher():
     ##best driver...code there is
-    auto_po(overseas_client_dict["lgd_yeontai"]["id"], overseas_client_dict["lgd_yeontai"]["pw"], overseas_client_dict["lgd_yeontai"]["identity"])
-    auto_po(overseas_client_dict["lgd_vh"]["id"], overseas_client_dict["lgd_vh"]["pw"], overseas_client_dict["lgd_vh"]["identity"])
+    retriever_art()
+    for client in overseas_client_list:
+        auto_po(client["id"], client["pw"], client["identity"])
+    # auto_po(overseas_client_dict["lgd_yeontai"]["id"], overseas_client_dict["lgd_yeontai"]["pw"], overseas_client_dict["lgd_yeontai"]["identity"])
+    # auto_po(overseas_client_dict["lgd_vh"]["id"], overseas_client_dict["lgd_vh"]["pw"], overseas_client_dict["lgd_vh"]["identity"])
 
 michael_schumacher()
---
+++ pograbber.bat
@@ -0,0 +1,6 @@
+@echo off
+ECHO ==========================
+ECHO Starting PO Retriever
+ECHO ============================
+"C:\Users\adrian\AppData\Local\Microsoft\WindowsApps\python3.10.exe" ""C:\Users\adrian\Desktop\scripts\po_check\po_bot.py""
+pause
